1c17ef7ff01f04fc9e9d4db8607e79ae3a9ae8e6
Chun <wongkaichun1996@gmail.com>
2017-11-17 17:59:07 +0800

    Test image + text reply

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -356,8 +356,14 @@ public class KitchenSinkController {
             }
             case "redeem":{
 	            	try {
+	            		List<Message> messageList = Arrays.asList();
+	            		ImageMessage coupon_jpg = new ImageMessage("https://help.idevaffiliate.com/wp-content/uploads/2015/04/coupon-graphic.gif", "https://help.idevaffiliate.com/wp-content/uploads/2015/04/coupon-graphic.gif");
+	            		messageList.add(coupon_jpg);
 	            		String result = database.redeem(userId);
-	            		this.replyText(replyToken, result + database.waterNotif(userId));
+	            		TextMessage text_message = new TextMessage(result);
+	            		messageList.add(text_message);
+	            		//this.replyText(replyToken, result + database.waterNotif(userId));
+	            		this.reply(replyToken, messageList);
 	            	} catch (Exception e) {
 	            		this.replyText(replyToken, "Sorry, please enter a valid input.");
 	            	};
